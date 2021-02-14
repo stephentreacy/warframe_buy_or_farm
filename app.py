1aa0b13833f96ff9bdcd87fcbfe7d10f8fe31a08
stephentreacy <stephen.treacy92@gmail.com>
2021-02-14 17:28:42 +0000

    Tidy and improve existing code

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 
 from flask import Flask, render_template, request
 import warframe_buy_or_farm as wf
+import pandas as pd
+import time
 
 app = Flask(__name__)
 
@@ -17,24 +19,39 @@ def prime_items():
     if request.method == 'POST':
         #Use entered URL to get items
         item_orders = {}
-        print(request.form)
+
         items = wf.get_item_list(request.form['url_tenno'])
-            
+
         for item in items:
-            item_dict = wf.get_market_prices(item)
-            if item_dict:
-                item_orders.update(item_dict)
+            orders = wf.get_market_prices(item, 'item')
+            if orders:
+                df_orders = pd.json_normalize(orders['orders'])
+                df_sell = df_orders[(df_orders['user.status'] == 'ingame') & (df_orders['order_type'] == 'sell')]
+                df_buy = df_orders[(df_orders['user.status'] == 'ingame') & (df_orders['order_type'] == 'buy')]
+                sell_orders = df_sell.nsmallest(5, 'platinum')['platinum'].values
+                buy_orders = df_buy.nlargest(5, 'platinum')['platinum'].values
+                item_orders.update({item : [sell_orders, buy_orders, item.lower().replace(' ','_').replace('-','_').replace("'",'').replace('&','and')]})
+
+            time.sleep(0.4)
 
         return render_template('items_page.html', items=item_orders)
     else:
         #Display text box for link
         return render_template('items_page.html', items=[])
 
-@app.route('/mods')
+@app.route('/mods', methods=['GET', 'POST'])
 def mods():
     """Lists mods and statistics on webpage"""
+    if request.method == 'POST':
+        #Get updated prices and reload
+        pass
 
-    return render_template('mods_page.html', tables=wf.mods_df())
+    else:
+        df_mod_stats = pd.read_csv("mod_stats.csv")
+        df_mod_stats['url'] = df_mod_stats['name'].str.lower().str.replace(' ','_').str.replace('-','_').str.replace("'",'').str.replace('&','and')
+        df_mod_stats['url'] = "<a href='https://warframe.market/items/" + df_mod_stats['url'] + "'>" + df_mod_stats['name'] + '</a>'
+        html = df_mod_stats[['url','datetime','rarity','avg_price','volume']].to_html(escape=False)
+        return render_template('mods_page.html', tables=html)
 
 
 if __name__ == '__main__':
--- warframe_buy_or_farm.py
+++ warframe_buy_or_farm.py
@@ -1,4 +1,5 @@
 """Warframe Buy or Farm
+
 Takes a Tenno Zone link from a file
 Gets the items selected via checkboxes on website
 Shows the price of the items from warframe.market"""
@@ -10,17 +11,64 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import pandas as pd
 
-def get_tenno_url():
-    """Gets personal https://tenno.zone/planner/ URL from the text file"""
+def get_mod_dataframe():
+    """Returns a dataframe containing all tradable mods"""
     
-    with open(r'tenno_url.txt') as url_file:
-        url = url_file.read()
+    #URL of JSON file on github that gets regularly updated
+    url = 'https://raw.githubusercontent.com/WFCD/warframe-items/development/data/json/Mods.json'
+    
+    try:
+        json_raw = urllib.request.urlopen(url)          
+        data = json.loads(json_raw.read())
+    except:
+        print('Error getting mod list')
+        df_mods = pd.DataFrame()
+    else:
+        df_mods = pd.DataFrame(data)
+        df_mods = df_mods[df_mods['tradable']]
+        df_mods.drop_duplicates(subset=['name'], inplace=True, ignore_index=True)
+    finally:
+        return df_mods[['name','rarity']]
+
+def create_mod_stats_file():
+
+    df_mod_stats = pd.DataFrame()
+
+    df_mods = get_mod_dataframe()
+
+    for mod in df_mods.values:
+
+        stats = get_market_prices(mod[0])
         
-    return url
+        if stats:
+            df_stats = pd.json_normalize(stats['statistics_closed']['90days'])
+            try:
+                df_stats['datetime']= pd.to_datetime(df_stats['datetime'])
+                latest_series = df_stats.iloc[df_stats['datetime'].idxmax()]
+                latest_series['name'] = mod[0]
+                latest_series['rarity'] = mod[1]
+                latest_series['datetime'] = latest_series['datetime'].date()
+                df_mod_stats  = df_mod_stats.append(latest_series)
+            except:
+                print("There's something wrong with" + mod[0])
+
+        time.sleep(0.4)
+
+    df_mod_stats.to_csv('mod_stats.csv', index=False)
+
+def get_mods_stats_file():
+    df_mod_stats = pd.read_csv("mod_stats.csv")
+    df_mod_stats['url'] = df_mod_stats['name'].str.lower().str.replace(' ','_').str.replace('-','_').str.replace("'",'').str.replace('&','and')
+    df_mod_stats['url'] = "<a href='https://warframe.market/items/" + df_mod_stats['url'] + "'>" + df_mod_stats['name'] + '</a>'
+    html = df_mod_stats.to_html(escape=False)
+    return html
 
 def get_item_list(url):
-    """Returns the list of items with selected checkboxes from URL"""
-    
+    """Returns the list of items from tenno.zone link.
+
+    PARAMETERS:
+        url -  URL of the tenno.zone link"""
+
     item_names = []
 
     #Open the URL as a headless Firefox instance
@@ -33,7 +81,6 @@ def get_item_list(url):
         browser.get(url)
     except:
         print(f'Error connecting to {url}')
-        item_names = []
     else:
         expand_checkbox = browser.find_element_by_xpath("//*[contains(text(), 'Include vaulted sets')]")
         browser.execute_script("arguments[0].click();", expand_checkbox.find_element_by_tag_name('input'))
@@ -43,91 +90,55 @@ def get_item_list(url):
         for item in items:
             parts = item.find_elements_by_tag_name('label')
             for part in parts:
-                if part is parts[0]:
-                    item_name = part.text.title()
+                if part is parts[0]: item_name = part.text.title()
+
                 if part.find_element_by_tag_name('input').is_selected():
                     if item_name != part.text.title():
-                        if part.text.title().find('Neuroptics') > -1:
-                            item_names.append(item_name + ' Neuroptics')
-                        elif part.text.title().find('Systems') > -1:
-                            item_names.append(item_name + ' Systems')
-                        elif part.text.title().find('Chassis') > -1:
-                            item_names.append(item_name + ' Chassis')
-                        else:
-                            item_names.append(item_name + ' ' + part.text.title())              
-                    else:
-                        item_names.append(item_name + ' Set')
+                        if part.text.title().find('Neuroptics') > -1: item_names.append(item_name + ' Neuroptics')
+                        elif part.text.title().find('Systems') > -1: item_names.append(item_name + ' Systems')
+                        elif part.text.title().find('Chassis') > -1: item_names.append(item_name + ' Chassis')
+                        else: item_names.append(item_name + ' ' + part.text.title())              
+                    else: item_names.append(item_name + ' Set')
     finally:            
         browser.quit()
         
     return item_names
 
-def get_market_prices(item):
-    """Create a dictionary for item with the highest 4 buy orders, lowest 4 sell orders and URL for item"""
-    
+def get_market_prices(item, type='mod'):
+    """Create a dictionary for statistics or orders for the item"""
+
     api_url = 'https://api.warframe.market/v1/items/'
     name_url = item.lower().replace(' ','_').replace('-','_').replace("'",'').replace('&','and')
-    url_item = api_url + name_url + '/orders'
-    item_orders = {}
-    
+    catagory = 'statistics' if type == 'mod' else 'orders'
+    url_item = api_url + name_url + '/' + catagory
+
     try:
         json_url_item = urllib.request.urlopen(url_item)          
     except:
         print(f'Item: {item} not found')
-        print('Kubrow Items not supported')
     else:
-        data_item = json.loads(json_url_item.read())
-        payload_orders = data_item['payload']['orders']
+        item_json = json.loads(json_url_item.read())
+        return item_json['payload']
 
-        sells_all = []
-        buys_all = []
-
-        for order in payload_orders:
-            if order['user']['status'] == 'ingame':
-                if order['order_type'] == 'buy':
-                    buys_all.append(order['platinum'])
-                if order['order_type'] == 'sell':
-                    sells_all.append(order['platinum'])
-
-        buying = sub_list(buys_all, 'buy')
-        selling = sub_list(sells_all)
-        time.sleep(.5)
-        item_orders[item] = [buying,selling,name_url]
-        
-        return item_orders
-
-def sub_list(orders, order_type='sell'):
-    '''Sorts the list and gets the 4 highest buy orders or 4 lowest sell orders'''
-    
-    if order_type == 'sell':
-        orders.sort()
-    else:
-        orders.sort(reverse=True)
-        
-    if len(orders) < 5:
-        return orders
-    else:
-        return orders[:4]
-
-def mods_df():
-    df_mod_stats = pd.read_csv("mod_stats.csv")
-    df_mod_stats['url'] = df_mod_stats['name'].str.lower().str.replace(' ','_').str.replace('-','_').str.replace("'",'').str.replace('&','and')
-    df_mod_stats['url'] = "<a href='https://warframe.market/items/" + df_mod_stats['url'] + "'>" + df_mod_stats['name'] + '</a>'
-    html = df_mod_stats.to_html(escape=False)
-    return html
 
 
 if __name__ == '__main__':
 
-    tenno_url = get_tenno_url()
-    items = get_item_list(tenno_url)
-    print('Items Retrieved')
     item_orders = {}
-    for item in items:
-        item_dict = get_market_prices(item)
-        if item_dict:
-            item_orders.update(item_dict)
 
+    items = get_item_list('https://tenno.zone/planner/Sk1Snn14E')
+    print('Items Retrieved', items)
+
+    for item in items:
+        orders = get_market_prices(item, 'item')
+        if orders:
+            df_orders = pd.json_normalize(orders['orders'])
+            df_sell = df_orders[(df_orders['user.status'] == 'ingame') & (df_orders['order_type'] == 'sell')]
+            df_buy = df_orders[(df_orders['user.status'] == 'ingame') & (df_orders['order_type'] == 'buy')]
+            sell_orders = df_sell.nsmallest(5, 'platinum')['platinum'].values
+            buy_orders = df_buy.nlargest(5, 'platinum')['platinum'].values
+            item_orders.update({item : [sell_orders, buy_orders, item.lower().replace(' ','_').replace('-','_').replace("'",'').replace('&','and')]})
+        time.sleep(0.4)
     print(item_orders)
 
             
